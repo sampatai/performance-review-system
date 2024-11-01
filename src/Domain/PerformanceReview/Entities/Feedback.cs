df1b92befd6a14c8ea5e31838c20715648be7a71
sampat1 <sampat.adhikari@gmail.com>
2024-11-01 12:53:22 +0545

    added functions in feedback

--- src/Domain/PerformanceReview/Entities/Feedback.cs
+++ src/Domain/PerformanceReview/Entities/Feedback.cs
@@ -1,4 +1,6 @@
-﻿using OfficePerformanceReview.Domain.PerformanceReview.Enums;
+﻿using OfficePerformanceReview.Domain.Common.ValueObjects;
+using OfficePerformanceReview.Domain.PerformanceReview.Enums;
+using OfficeReview.Domain.Questions.Entities;
 
 namespace OfficePerformanceReview.Domain.PerformanceReview.Entities
 {
@@ -6,17 +8,67 @@ namespace OfficePerformanceReview.Domain.PerformanceReview.Entities
     {
         protected Feedback()
         {
-            
+
         }
         public Guid FeedbackGuid { get; private set; }
-        private List<BehaviorMetric> _BehaviorMetrics = new();
-        public IReadOnlyList<BehaviorMetric> BehaviorMetrics => _BehaviorMetrics.AsReadOnly();
+        private List<BehaviorMetric> _behaviorMetrics = new();
+        public IReadOnlyList<BehaviorMetric> BehaviorMetrics => _behaviorMetrics.AsReadOnly();
         public string? RevieweeComment { get; set; }
         public string? ReviewerComment { get; set; }
         public OverallRating? PotentialLevel { get; private set; }
         public FeedbackStatus RevieweeFeedbackStatus { get; private set; }
-        public FeedbackStatus ReviewerFeedbackStatus { get; private set; }
+        public FeedbackStatus? ReviewerFeedbackStatus { get; private set; }
 
+        public Feedback(string? revieweeComment, FeedbackStatus revieweeFeedbackStatus)
+        {
+            Guard.Against.Null(revieweeFeedbackStatus, nameof(revieweeFeedbackStatus));
 
+            FeedbackGuid = Guid.NewGuid();
+            RevieweeComment = revieweeComment;
+            RevieweeFeedbackStatus = revieweeFeedbackStatus;
+
+        }
+
+        internal void AddBehaviorMetricByReviewee(QuestionFeedback question,
+            RatingScale ratingScale,
+            string revieweeRemarks)
+        {
+            Guard.Against.Null(question, nameof(question));
+
+            _behaviorMetrics.Add(new BehaviorMetric(question, ratingScale, revieweeRemarks));
+        }
+        internal void SetBehaviorMetricByReviewee(Guid metricGUID,
+           RatingScale ratingScale,
+           string revieweeRemarks)
+        {
+            var single = _behaviorMetrics.SingleOrDefault(x => x.MetricGUID == metricGUID)!;
+            single.SetBehaviorMetric(ratingScale, revieweeRemarks);
+        }
+
+        internal void SetBehaviorMetricByReviewer(Guid metricGUID,
+           RatingScale ratingScale,
+           string reviewerRemarks)
+        {
+            var single = _behaviorMetrics.SingleOrDefault(x => x.MetricGUID == metricGUID)!;
+            single.SetReviewerRemarks(reviewerRemarks, ratingScale);
+        }
+        internal void SetReviewer(FeedbackStatus feedbackStatus,
+            string reviewerComment,
+            OverallRating rating)
+        {
+            Guard.Against.Null(feedbackStatus, nameof(feedbackStatus));
+            Guard.Against.Null(rating, nameof(rating));
+
+            ReviewerFeedbackStatus = feedbackStatus;
+            PotentialLevel = rating;
+            ReviewerComment = reviewerComment;
+        }
+        internal void SetReviewee(string revieweeComment, FeedbackStatus feedbackStatus)
+        {
+            Guard.Against.Null(feedbackStatus, nameof(feedbackStatus));
+
+            RevieweeFeedbackStatus = feedbackStatus;
+            RevieweeComment = revieweeComment;
+        }
     }
 }
--- src/Domain/PerformanceReview/Enums/FeedbackStatus.cs
+++ src/Domain/PerformanceReview/Enums/FeedbackStatus.cs
@@ -4,7 +4,7 @@
     {
         public static readonly FeedbackStatus Pending = new FeedbackStatus(1, "Pending");
         public static readonly FeedbackStatus Completed = new FeedbackStatus(2, "Completed");
-     
+        public static readonly FeedbackStatus Started = new FeedbackStatus(3, "Started");
         public FeedbackStatus(int id, string name) : base(id, name)
         {
         }
