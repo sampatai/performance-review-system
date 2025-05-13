a5c7c2085be6c3cab23dcb06440287211b734f7f
sampat1 <sampat.adhikari@gmail.com>
2025-05-13 16:20:12 +1000

    added repository

---
+++ src/Application/Common/Repository/IEvaluationFormTemplateRepository.cs
@@ -0,0 +1,12 @@
+﻿using OfficeReview.Domain.Questions.Root;
+using OfficeReview.Shared.SeedWork;
+
+namespace OfficePerformanceReview.Application.Common.Repository
+{
+    public interface IReadonlyQuestionRepository : IReadOnlyRepository<EvaluationFormTemplate>
+    {
+    }
+    public interface IEvaluationFormTemplateRepository : IRepository<EvaluationFormTemplate>
+    {
+    }
+}
